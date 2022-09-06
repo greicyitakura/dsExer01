83bc75a0cc95adf206fc2f54999bf97579b04c79
Greicy Itakura <greicyitakura@gmail.com>
2022-09-06 14:09:44 -0300

    Project created

---
+++ exer01/src/main/java/com/greicy/exer01/Exer01Application.java
@@ -0,0 +1,13 @@
+package com.greicy.exer01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exer01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exer01Application.class, args);
+	}
+
+}
---
+++ exer01/src/test/java/com/greicy/exer01/Exer01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greicy.exer01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exer01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
